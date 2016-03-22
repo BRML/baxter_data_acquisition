bfbddaf3603dc2f0be3b338e0e7cdcc37bf63b94
lude-ma <ludersdorfer@fortiss.org>
2016-03-22 15:17:22 +0100

    depth image snappy compression is working

--- src/recorder/depth_recorder.py
+++ src/recorder/depth_recorder.py
@@ -30,13 +30,17 @@ import numpy as np
 import rospy
 import snappy
 import struct
-import sys
 
 from sensor_msgs.msg import Image
 
 
 class DepthRecorder(object):
     def __init__(self):
+        """ Depth recorder class writing depth ROS image messages (single
+        channel Float32 image) recorded with, e.g., the Kinect V2 sensor,
+        compressed into a .bin binary file and timestamps for each image
+        frame into an accompanying .txt file.
+        """
         self._fp_d = None
         self._fp_ts = None
         self._sub = None
@@ -44,6 +48,12 @@ class DepthRecorder(object):
         self.camera = '/cameras/kinect/depth/image_raw'
 
     def start(self, outname):
+        """ Set up the depth recorder with the parameters for the recording
+        and subscribe to the callback function of the depth sensor.
+        :param outname: Filename to write the binary and text files to,
+        without the extension.
+        :return: Whether the binary- and text file were opened successfully.
+        """
         try:
             self._fp_ts = open(outname + '_depth.txt', 'w')
         except IOError:
@@ -70,19 +80,27 @@ class DepthRecorder(object):
         except cv_bridge.CvBridgeError:
             print 'ERROR-add_image-Problem with ROS image message conversion.'
             raise
-        # convert float32 image to int8 image
+
+        # Scale float32 image to uint16 image.
+        # '{min, max}_cutoff are the minimum and maximum range of the depth
+        # sensor of the Kinect V2.
         min_cutoff = 0.5
         max_cutoff = 4.5
-        img_int8 = 255*(img_float32 - min_cutoff)/(max_cutoff - min_cutoff)
-        img_int8 = img_int8.astype(np.uint8, copy=False)
+        img_uint16 = 65535*(img_float32 - min_cutoff)/(max_cutoff - min_cutoff)
+        img_uint16 = img_uint16.astype(np.uint16, copy=False)
+
         # compress image with snappy
-        img_comp = snappy.compress(img_int8)
+        img_comp = snappy.compress(img_uint16)
         # write number of bytes of compressed image
-        self._fp_d.write("%s" % sys.getsizeof(img_comp))
+        nr_bytes = struct.pack('<L', len(img_comp))
+        self._fp_d.write(nr_bytes)
         # write compressed image
         self._fp_d.write(img_comp)
 
     def stop(self):
+        """ Stop recording data from the depth sensor.
+        :return: Whether the binary- and text file are open.
+        """
         self._sub.unregister()
         self._fp_d.close()
         self._fp_ts.close()
@@ -100,11 +118,12 @@ class DepthRecorder(object):
         self._camera = camera
 
 
-def depth_from_binary(binary_name):
+def depth_from_binary(binary_name, imgsize=(240, 320)):
     """ Decode binary file containing depth images and return the depth
     images as a numpy ndarray.
     :param binary_name: The file name of the binary file to read.
-    :return: numpy array containing 'l' images of size 240x320.
+    :param imgsize: The size (height, width) of each uncompressed image.
+    :return: numpy array containing 'l' images of size 'imgsize'.
     """
     images = list()
     with open(binary_name, 'rb') as fp:
@@ -117,18 +136,18 @@ def depth_from_binary(binary_name):
     l = len(images)
     images = np.array(images)
     images = np.fromstring(images, dtype=np.dtype('>u2'))
-    return images.reshape((l, 240, 320))
+    return images.reshape((l,) + imgsize)
 
 
 if __name__ == '__main__':
     import cv2
 
     # fn = '/home/baxter/Downloads/DepthSenseDepthLog2015-12-17 13.13.19.647.bin'
-    fn = '/home/baxter/ros_ws/src/baxter_data_acquisition/data/201603221253-0_kinect_depth_depth.bin'
+    fn = '/home/baxter/ros_ws/src/baxter_data_acquisition/data/201603221452-0_kinect_depth_depth.bin'
 
-    images = depth_from_binary(fn)
+    imgs = depth_from_binary(fn, (424, 512))
 
-    for i in range(images.shape[0]):
-        cv2.imshow('depthimage', images[i, :, :])
+    for i in range(imgs.shape[0]):
+        cv2.imshow('depthimage', imgs[i, :, :])
         cv2.waitKey(0)
     cv2.destroyAllWindows()
