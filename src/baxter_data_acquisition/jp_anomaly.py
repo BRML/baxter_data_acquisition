27027b58830ff7abc3370257f7975d3bcdf96887
lude-ma <ludersdorfer@fortiss.org>
2016-04-06 13:28:32 +0200

    (external) kinect recorder added

--- src/baxter_data_acquisition/jp_anomaly.py
+++ src/baxter_data_acquisition/jp_anomaly.py
@@ -60,6 +60,7 @@ from recorder import (
     CameraRecorder,
     FlashRecorder,
     JointRecorder,
+    KinectRecorder
 )
 
 
@@ -114,7 +115,7 @@ class JointPosition(object):
             self._camera = baxter_interface.CameraController('head_camera')
             self._rec_cam = CameraRecorder()
         if self._threed:
-            # TODO: set up Kinect recorder instance here
+            self._rec_kinect = KinectRecorder()
             self._rec_flash = FlashRecorder()
 
         self._pub_rate = rospy.Publisher('robot/joint_state_publish_rate',
@@ -192,12 +193,14 @@ class JointPosition(object):
                                         self._camera.fps,
                                         self._camera.resolution)
                 if self._threed:
+                    self._rec_kinect.start(outfile + '-%i_kinect' % nr)
                     self._rec_flash.start(outfile + '-%i_flash_white' % nr)
                 flash_screen(3, 0.5, 0.5)
                 self._one_sample()
                 if self._images:
                     self._rec_cam.stop()
                 if self._threed:
+                    self._rec_kinect.stop()
                     self._rec_flash.stop()
                 self._rec_joint.stop()
                 self._rec_joint.write_sample()
