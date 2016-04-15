7cc0f27b52c1c5e0643882fb5889ac50295e2db7
lude-ma <ludersdorfer@fortiss.org>
2016-04-15 08:52:23 +0200

    documentation

--- src/recorder/__init__.py
+++ src/recorder/__init__.py
@@ -2,10 +2,14 @@
 
 """ Module for recorders recording data with the baxter research robot.
 
-Implements a camera recorder for recording videos and time-stamps for the
+Implements a camera recorder for recording videos and time stamps for the
 image frames from the robot's head camera, as well as a joint recorder for
 recording joint configurations, joint torques, Cartesian acceleration as well
 as anomaly commands from one of the robot's limbs.
+Furthermore, a flash recorder recording time stamps of screen flashes and
+two depth recorders (Kinect V2 and Senz3d) recording rgb data analogous to
+the camera recorder and depth data as compressed binary files with associated
+time stamp files are implemented.
 """
 
 from camera_recorder import (
