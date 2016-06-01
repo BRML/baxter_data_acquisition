9dcf88f06c0533b854bb4e3dc2e77df10c17a93f
lude-ma <ludersdorfer@fortiss.org>
2016-06-01 18:25:30 +0200

    documentation

--- src/recorder/queue_subscriber.py
+++ src/recorder/queue_subscriber.py
@@ -30,64 +30,100 @@ from threading import Thread
 
 class QueueSubscriber(Thread):
     def __init__(self, topic, msg_type, callback):
+        """ A ROS subscriber augmented with a FIFO queue for messages.
+
+        A queue subscriber is a thread which starts a ROS subscriber on a
+        given topic and message type and puts the received ROS messages in a
+        FIFO queue. Upon receiving the stop command, the queue subscriber
+        un-subscribes from the topic and processes the messages in the queue
+        with a given callback function. Once the queue is empty, the thread
+        joins.
+        :param topic: The ROS topic to subscribe to.
+        :param msg_type: The ROS message type to use.
+        :param callback: The processing function to apply to the received
+            ROS messages.
+        """
         super(QueueSubscriber, self).__init__()
         self._topic = topic
         self._msg_type = msg_type
         self._callback = callback
         # A FIFO queue of infinite size
         self._queue = Queue.Queue(maxsize=0)
+        # The ROS subscriber
         self._sub = None
+        # Variables for performance measuring
         self._count = 0
         self._t_start = None
+        self._t_duration = None
 
     def __str__(self):
-        return rospy.get_caller_id()
+        return "QueueSubscriber on {}".format(rospy.get_caller_id())
 
     def clean_shutdown(self):
-        rospy.loginfo("'%s' Initiating shutdown ..." % self)
+        """ Clean shutdown of the queue subscriber. """
+        rospy.loginfo("'%s' Initiate shutdown ..." % self)
         self._ros_unsubscribe()
         self._queue = None
+        self.join(timeout=1.0)
         rospy.loginfo("'%s' ... shutdown completed." % self)
 
     def _ros_unsubscribe(self):
-        if self._sub is not None:
-            rospy.loginfo("'%s' Unregister subscriber ..." % self)
+        """ If we have subscribed to a ROS topic, un-subscribe from it. """
+        if self._sub:
+            rospy.loginfo("'%s' Un-register ROS subscriber ..." % self)
             self._sub.unregister()
             self._sub = None
-            rospy.loginfo("'%s' ... unregistered subscriber." % self)
+            rospy.loginfo("'%s' ... unregistered ROS subscriber." % self)
 
     def run(self):
+        """ The routine called by thread.start().
+        If not yet running, start the queue subscriber by subscribing to the
+        ROS topic.
+        :raise: RuntimeError if queue subscriber is already running (thread
+            is already started).
+        """
         if not self._sub:
-            rospy.loginfo("'%s' Start QueueSubscriber." % self)
+            rospy.loginfo("'%s' Register ROS subscriber ..." % self)
             self._count = 0
             self._t_start = rospy.get_time()
             self._sub = rospy.Subscriber(self._topic, self._msg_type,
                                          callback=self._ros_callback,
                                          queue_size=100)
-            return True
-        else:
-            rospy.loginfo("'%s' QueueSubscriber already running.")
-            return False
+            rospy.loginfo("'%s' ... registered ROS subscriber." % self)
 
     def _ros_callback(self, msg):
+        """ Callback for the ROS subscriber.
+        Puts received messages into a FIFO queue.
+        """
         if self._queue:
             self._queue.put(msg)
 
     def stop(self):
+        """ If running, stop the queue recorder by un-subscribing the ROS
+        subscriber and processing the messages in the queue. Upon completion,
+        joins the thread (i.e., itself).
+        :return: Whether the thread (i.e., the instance) is alive.
+        :raise: RuntimeError if queue subscriber is not yet running (thread
+            has not been started).
+        """
         if not rospy.is_shutdown():
             self._ros_unsubscribe()
+            if self._t_start:
+                self._t_duration = rospy.get_time() - self._t_start
             rospy.loginfo("'%s' Process data queue ..." % self)
             while not rospy.is_shutdown() and not self._queue.empty():
-                next_msg = self._queue.get(block=False)  # Raises Queue.Empty if no item in queue
+                # Raises Queue.Empty if no item in queue. This should be
+                # prevented by checking self._queue.empty() above.
+                next_msg = self._queue.get(block=False)
                 self._callback(next_msg)
                 self._queue.task_done()
                 self._count += 1
             rospy.loginfo("'%s' ... processed data queue." % self)
             self._display_performance()
-        self.join(5.0)
+        self.join(timeout=5.0)
         return self.is_alive()
 
     def _display_performance(self):
         """ Log performance information (messages received). """
         rospy.loginfo("'%s' Received %d messages in %.2f s." %
-                      (self, self._count, rospy.get_time() - self._t_start))
+                      (self, self._count, self._t_duration))
